d8693d429a8dd4ccb9fc3e38a39a75b41f48f862
katerynanovik <77755224+katerynanovik@users.noreply.github.com>
2021-04-26 00:16:54 +0300

    Delete PODARUNOCHKI directory

--- PODARUNOCHKI/Frontend/src/Templates.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var ejs = require('ejs');
-
-exports.BoxChoice_OneItem = ejs.compile(fs.readFileSync('./Frontend/templates/item.ejs', "utf8"));
-
-exports.BoxCart_OneItem = ejs.compile(fs.readFileSync('./Frontend/templates/cartItem.ejs', "utf8"));
-
-exports.singleItem = ejs.compile(fs.readFileSync('./Frontend/templates/singleItem.ejs', "utf8"));
